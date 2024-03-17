373c9f39a44808005fdcad2998de163a29383403
harsha <121601031+harshabavigadda@users.noreply.github.com>
2024-03-17 10:49:37 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ app.use(cors({
   origin: ["http://127.0.0.1:5173"],
   methods: ["POST","GET","DELETE"],
   credentials: true
-  ));
+}));
 
 // mongoose.connect("mongodb://localhost:27017/algo");
 mongoose.connect("mongodb+srv://algozenith:nitc@cluster0.pknc4ob.mongodb.net/algozenith?retryWrites=true&w=majority");
